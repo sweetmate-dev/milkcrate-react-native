8031ca7263ec6ba0f17ca2053ee0493f87050681
sweetmate-dev <john.amuesi525@gmail.com>
2017-03-02 22:56:16 -0500

    minor edits

--- index.android.js
+++ index.android.js
@@ -1,17 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { AppRegistry } from 'react-native';
 import App from './app/js/app';
 
 AppRegistry.registerComponent('Milkcrate', () => App);
-
